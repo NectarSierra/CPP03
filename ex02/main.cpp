c271c15cf6ac1ba5b9873fdc39e2545b6fc94850
Nathan Saillez <nsaillez@shi-r6-p1.s19.be>
2025-09-23 15:26:31 +0200

    x

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 13:08:08 by nsaillez          #+#    #+#             */
-/*   Updated: 2025/09/23 15:18:39 by nsaillez         ###   ########.fr       */
+/*   Updated: 2025/09/23 15:22:38 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main( void )
 
 	FragTrap* b = new FragTrap("B");
 	b->highFivesGuys();
-	b->beRepaired(50);
+	b->beRepaired(UINT_MAX);
 	b->takeDamage(100);
 	b->attack("TEST");
 	b->takeDamage(1000);
